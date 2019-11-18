f01c45d1b6d3a0cc12e5e9d2e15349aff4ac22e1
james-mallory <james.p.mallory@gmail.com>
2019-11-17 21:56:20 -0600

    moves problems into their own directory and solves problem 1

---
+++ src/problems/1.js
@@ -0,0 +1,38 @@
+/*
+ 
+Multiples of 3 and 5
+
+Problem 1
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+ */
+
+import times from 'lodash/times';
+import some from 'lodash/some';
+
+import getIsMultipleOf from '../utils/factoring';
+
+const MAX = 1000;
+const MULTIPLE_CHECKS = [3, 5];
+
+let sum = 0;
+
+
+function solve() {
+    times(MAX, addToSum);
+    console.log(sum);
+}
+
+function addToSum(number) {
+    if (number === 0) return 0;
+
+    some(MULTIPLE_CHECKS, numberToCheckIfMultiple => {
+        if (getIsMultipleOf(number, numberToCheckIfMultiple)) {
+            sum += number;
+            return true;
+        }
+    });
+}
