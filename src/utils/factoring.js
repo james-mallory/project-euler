26093ac54981463d65ae77da86d5ae81a45481d8
james-mallory <james.p.mallory@gmail.com>
2019-11-17 23:45:15 -0600

    adds util function for figuring out if a number is prime

--- src/utils/factoring.js
+++ src/utils/factoring.js
@@ -1,3 +1,17 @@
-export default function getIsMultipleOf(A, B) {
+import some from 'lodash/some';
+
+export function getIsMultipleOf(A, B) {
     return A % B === 0;
-};
+}
+
+export function getIsPrime(number) {
+    const root = Math.sqrt(number);
+    let isPrime = true;
+    for (let i = 2; i < root; i++) {
+        if (getIsMultipleOf(number, i)) {
+            isPrime = false;
+            break;
+        }
+    }
+    return isPrime;
+}
