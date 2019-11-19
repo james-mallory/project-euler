dd70211ddd0f151cbf5f657f191c709d52440243
james-mallory <james.p.mallory@gmail.com>
2019-11-18 21:17:03 -0600

    improves results log

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 import forEach from 'lodash/forEach';
+import moment from 'moment';
 import fs from 'fs';
 
 const PROBLEMS_PATH = './src/problems';
+let start, end, count;
 
 function run() {
-    console.time(`Time for all problems`);
+    console.log(`~~~~~~~~~~~PROJECT EULER~~~~~~~~~~~`);
+    console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+    start = moment();
     fs.readdir(PROBLEMS_PATH, importAndRunAll);
 }
 
 function importAndRunAll(err, files) {
+    count = files.length;
     forEach(files, importAndRun);
-    console.timeEnd(`Time for all problems`);
+    end = moment();
+
+    displayResults();
 }
 
 function importAndRun(file) {
     const problem = require(`${PROBLEMS_PATH}/${file}`);
     problem.default();
+}
+
+function displayResults() {
+    const timeTaken = end.valueOf() - start.valueOf();
+    const avgPerProblem = timeTaken / count;
+
+    console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+    console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+    console.log(`Problems Solved: ${count}`);
+    console.log(`Average Time: ${avgPerProblem}ms`);
+    console.log(`Total Time: ${timeTaken}ms`);
 }
 
 run();
