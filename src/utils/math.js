a6eb1fb362812f27c2ab4aee76acb0161d3f99b7
james-mallory <james.p.mallory@gmail.com>
2019-11-18 22:17:42 -0600

    adds sum of squares and square of sums

--- src/utils/math.js
+++ src/utils/math.js
@@ -3,3 +3,23 @@ export function getIsPalindrome(number) {
     const reverseString = numberString.split(``).reverse().join(``);
     return numberString === reverseString;
 }
+
+export function getSumOfSquares(min, max) {
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += Math.pow(i, 2);
+    }
+    return sum;
+}
+
+export function getSquareOfSums(min, max) {
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return Math.pow(sum, 2);
+}
+
+export function getDiffSquareOfSumsSubSumOfSquares(min, max) {
+    return getSquareOfSums(min, max) - getSumOfSquares(min, max);
+}
