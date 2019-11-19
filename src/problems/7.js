17eb76c844d8dfd30397835c7d3758db6d50136b
james-mallory <james.p.mallory@gmail.com>
2019-11-18 22:56:30 -0600

    adds problem 7 solution

---
+++ src/problems/7.js
@@ -0,0 +1,17 @@
+/*
+ 
+10001st prime
+
+Problem 7
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+
+ */
+
+import { getNthPrime } from '../utils/factoring';
+
+export default function solve() {
+    console.log(`Solution #7: ${getNthPrime(10001)}`);
+}
