fbd5742c46eadcfc9dfb215074e251ec7c1330bb
james-mallory <james.p.mallory@gmail.com>
2019-11-19 23:06:21 -0600

    adds problem 8 solution

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,3 +1,6 @@
+import split from 'lodash/split';
+import forEach from 'lodash/forEach';
+
 export function getIsPalindrome(number) {
     const numberString = number.toString();
     const reverseString = numberString.split(``).reverse().join(``);
@@ -22,4 +25,35 @@ export function getSquareOfSums(min, max) {
 
 export function getDiffSquareOfSumsSubSumOfSquares(min, max) {
     return getSquareOfSums(min, max) - getSumOfSquares(min, max);
+}
+
+export function getMaxProductOfNAdjacentDigits(number, n) {
+    const numberString = number.toString();
+    const numberArray = split(numberString, ``);
+
+    let maxProduct = 0;
+    const nNumbersArray = [];
+    forEach(numberArray, numberAsString => {
+        const num = parseInt(numberAsString);
+        nNumbersArray.push(num); 
+
+        if (nNumbersArray.length > n) {
+            nNumbersArray.shift();
+        }
+
+        if (nNumbersArray.length === n) {
+            const product = getProductOfNumbers(nNumbersArray);
+            if (product > maxProduct) maxProduct = product;
+        }
+        
+    });
+    return maxProduct;
+}
+
+export function getProductOfNumbers(numbers) {
+    let product = 1;
+    forEach(numbers, number => {
+        product *= number;
+    });
+    return product;
 }
