59bd63d1055994cd53fe936aa178dd3fb64f7d8c
james-mallory <james.p.mallory@gmail.com>
2019-12-10 23:05:20 -0600

    adds constants for the number of letters in numbers

---
+++ src/utils/numberOfLetters.js
@@ -0,0 +1,42 @@
+export const ONES_DIGIT = [
+    0, // zero
+    3, // one
+    3, // two
+    5, // three
+    4, // four
+    4, // five
+    3, // six
+    5, // seven
+    5, // eight
+    4  // nine
+];
+
+export const TEENS = [
+    3, // ten
+    6, // eleven
+    6, // twelve
+    8, // thirteen
+    8, // fourteen
+    7, // fifteen
+    7, // sixteen
+    9, // seventeen
+    8, // eighteen
+    8  // nineteen
+];
+
+export const TENS_DIGIT = [
+    0, // zero
+    0, // teens
+    6, // twenty
+    6, // thirty
+    5, // forty
+    5, // fifty
+    5, // sixty
+    7, // seventy
+    6, // eighty
+    6  // ninety
+];
+
+export const HUNDRED = 7;
+export const THOUSAND = 8;
+export const AND = 3;
