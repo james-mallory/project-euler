172b25605e3e6cd7a171895d20bfada7a8f37d34
james-mallory <james.p.mallory@gmail.com>
2019-12-14 17:37:43 -0600

    adds day constants

---
+++ src/utils/days.js
@@ -0,0 +1,41 @@
+export const PerMonth = {
+    NOT_LEAP: [
+        31,
+        28, // February
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31
+    ],
+
+    LEAP: [
+        31,
+        29, // February
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31
+    ]
+};
+
+export const PER_WEEK = 7;
+
+export const SUNDAY = 0;
+export const MONDAY = 1;
+export const TUESDAY = 2;
+export const WEDNESDAY = 3;
+export const THURSDAY = 4;
+export const FRIDAY = 5;
+export const SATURDAY = 6;
